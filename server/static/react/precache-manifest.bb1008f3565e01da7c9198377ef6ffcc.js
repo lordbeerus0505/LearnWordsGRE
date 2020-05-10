4444f490b50b0add42154e2f468fba9d3ef96963
lordbeerus0505 <abhiram.natarajan@gmail.com>
2020-05-10 18:08:37 +0530

    OnSubmit not working, using onClick but changed cursor PART2

---
+++ server/static/react/precache-manifest.bb1008f3565e01da7c9198377ef6ffcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec70887bd0d006522a3bd59899cf12f",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "b268e5133640b70d4497",
+    "url": "/static/react/css/2.8f1c7784.chunk.css"
+  },
+  {
+    "revision": "6fe68504d7540203a7e1",
+    "url": "/static/react/css/main.ccdf2cd3.chunk.css"
+  },
+  {
+    "revision": "b268e5133640b70d4497",
+    "url": "/static/react/js/2.8a600aec.chunk.js"
+  },
+  {
+    "revision": "6fe68504d7540203a7e1",
+    "url": "/static/react/js/main.90ed188a.chunk.js"
+  },
+  {
+    "revision": "cf247243e51246a5ce10",
+    "url": "/static/react/js/runtime-main.7b9bd531.js"
+  },
+  {
+    "revision": "7507b0c775b607be7f7bc724be0405cb",
+    "url": "/static/react/media/trump.7507b0c7.gif"
+  }
+]);
