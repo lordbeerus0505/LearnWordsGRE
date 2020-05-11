cca97e587206c81d0cdc310d85d0c6531db55a7b
lordbeerus0505 <abhiram.natarajan@gmail.com>
2020-05-12 01:10:47 +0530

    Progress bar content changed

---
+++ server/static/react/precache-manifest.42d839ccae6bb729b631790dfc083893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d608aaf5f2a99360ce4d1831444df87",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "be46cb71af45c387ebd2",
+    "url": "/static/react/css/2.8f1c7784.chunk.css"
+  },
+  {
+    "revision": "f88fba5865ae7914ca8c",
+    "url": "/static/react/css/main.d9c3ed85.chunk.css"
+  },
+  {
+    "revision": "be46cb71af45c387ebd2",
+    "url": "/static/react/js/2.c339189f.chunk.js"
+  },
+  {
+    "revision": "f88fba5865ae7914ca8c",
+    "url": "/static/react/js/main.bb7833ba.chunk.js"
+  },
+  {
+    "revision": "cf247243e51246a5ce10",
+    "url": "/static/react/js/runtime-main.7b9bd531.js"
+  },
+  {
+    "revision": "7507b0c775b607be7f7bc724be0405cb",
+    "url": "/static/react/media/trump.7507b0c7.gif"
+  }
+]);
