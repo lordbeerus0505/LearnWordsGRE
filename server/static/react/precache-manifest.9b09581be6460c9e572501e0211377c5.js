5ae0ff0bcb85c8477d2519d1282179b9da9cdf21
lordbeerus0505 <abhiram.natarajan@gmail.com>
2020-05-11 12:44:20 +0530

    Sorting done on the front end

---
+++ server/static/react/precache-manifest.9b09581be6460c9e572501e0211377c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dacc25628a3152cd8914e2dc541cf7",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "be46cb71af45c387ebd2",
+    "url": "/static/react/css/2.8f1c7784.chunk.css"
+  },
+  {
+    "revision": "7a122a532cd809e69aa5",
+    "url": "/static/react/css/main.1fb9efc4.chunk.css"
+  },
+  {
+    "revision": "be46cb71af45c387ebd2",
+    "url": "/static/react/js/2.c339189f.chunk.js"
+  },
+  {
+    "revision": "7a122a532cd809e69aa5",
+    "url": "/static/react/js/main.04c3dedd.chunk.js"
+  },
+  {
+    "revision": "cf247243e51246a5ce10",
+    "url": "/static/react/js/runtime-main.7b9bd531.js"
+  },
+  {
+    "revision": "7507b0c775b607be7f7bc724be0405cb",
+    "url": "/static/react/media/trump.7507b0c7.gif"
+  }
+]);
